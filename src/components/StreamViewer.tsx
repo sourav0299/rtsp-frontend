56d6f4314cb77ca3d6ef1e1e5acffa4c85efb32e
Sourav Kumar <105085605+sourav0299@users.noreply.github.com>
2025-05-18 16:14:54 +0530

    Update StreamViewer.tsx

--- src/components/StreamViewer.tsx
+++ src/components/StreamViewer.tsx
@@ -20,7 +20,7 @@ export default function StreamViewer({ streamUrl }: StreamViewerProps) {
   }, [socket]);
 
   const startStream = () => {
-    const ws = new WebSocket(`wss://rtsp-backend.onrender.com/ws/stream/`);
+    const ws = new WebSocket(`ws://rtsp-backend.onrender.com/ws/stream/`);
     setSocket(ws);
 
     let lastTime = Date.now();
